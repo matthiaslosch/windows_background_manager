5dc65a9354001471cc65bb49d658dc65b90ec73f
Matthias Losch <losch@hey.com>
2020-09-04 17:29:23 +0200

    Link with kernel32.lib
    Be explicit.

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 
 IF NOT EXIST build mkdir build
 pushd build
-cl /nologo /Zi /O2 /Fewindows_background_manager.exe ..\main.c /link user32.lib advapi32.lib
+cl /nologo /Zi /O2 /Fewindows_background_manager.exe ..\main.c /link kernel32.lib user32.lib advapi32.lib
 popd
